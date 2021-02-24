28dd5319b3584331b73a0093acfa3b7eff2f7f8e
EduGomeTI <edugomesti@outlook.com>
2021-02-24 17:05:44 -0300

    Remove department

--- Udemy/Nelio Alves/Java/curso_programacao/workshop-javafx-jdbc/src/model/dao/impl/DepartmentDaoJDBC.java
+++ Udemy/Nelio Alves/Java/curso_programacao/workshop-javafx-jdbc/src/model/dao/impl/DepartmentDaoJDBC.java
@@ -72,7 +72,7 @@ public class DepartmentDaoJDBC implements DepartmentDao{
 	@Override
 	public void deleteById(Integer id) {
 		try {
-		st = conn.prepareStatement("DELETE department WHERE Id = ?");
+		st = conn.prepareStatement("DELETE FROM department WHERE Id = ?");
 		st.setInt(1, id);
 		
 		st.executeUpdate();
