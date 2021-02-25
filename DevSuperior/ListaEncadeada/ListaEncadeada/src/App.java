58b96b584063cabce10463bf8bb5524805285de2
EduGomeTI <edugomesti@outlook.com>
2021-02-25 15:24:56 -0300

    Listas Encadeadas DevSuperior

---
+++ DevSuperior/ListaEncadeada/ListaEncadeada/src/App.java
@@ -0,0 +1,50 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+import util.List;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        List list = new List();
+
+        int op = 0;
+        do {
+            showMenu();
+            op = sc.nextInt();
+
+            switch (op) {
+                case 1: {
+                    System.out.print("Digite um número: ");
+                    double value = sc.nextDouble();
+                    list.add(value);
+                    break;
+                }
+                case 2: {
+                    System.out.println(list.toString());
+                    break;
+                }
+                case 3: {
+                    System.out.println("Fim do programa!");
+                    break;
+                }
+                default: {
+                    System.out.println("Opção Inválida!");
+                }
+            }
+
+        } while (op != 3);
+
+        sc.close();
+
+    }
+
+    public static void showMenu() {
+        System.out.println("1 - Inserir elemento na lista");
+        System.out.println("2 - Mostrar lista");
+        System.out.println("3 - Sair");
+    }
+}
